180db8f4af1bb5c4d620bebb4fc431b113698378
Ponk8 <lucas.alencar31@gmail.com>
2024-01-03 16:31:40 -0300

    feat: use react-router-dom for routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Outlet } from 'react-router-dom'
-
 import Header from './components/Header'
 
 function App() {
