d987a50be9e8c7989ac1a5d1ce5d70cde24a87ae
Ponk8 <lucas.alencar31@gmail.com>
2024-01-16 15:33:52 -0300

    chore

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://lucas-avs.github.io/',
+  base: '/Lucas-AVS.github.io/',
   plugins: [react()],
 })
